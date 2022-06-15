49973d59c20149d835d9cc89177fcbbf9c17eaa2
elyssadg <elyssa.davina@gmail.com>
2022-06-15 22:09:28 +0700

    edit

--- script/contact.js
+++ script/contact.js
@@ -6,6 +6,9 @@ let message = document.getElementById("message");
 let errorUser = document.getElementById("errorUser");
 let errorPhone = document.getElementById("errorPhone");
 let errorEmail = document.getElementById("errorEmail");
+let errorSubject = document.getElementById("errorSubject");
+let errorMessage = document.getElementById("errorMessage");
+
 function validateForm(){
     if(username.value.length<2){
         errorUser.innerHTML = "Username is shorter than 2 characters"
@@ -21,5 +24,15 @@ function validateForm(){
         errorEmail.innerHTML="Email must end with @gmail.com"
     }else{
         errorEmail.innerHTML=""
+    }
+    if (subject.value.length == 0){
+        errorSubject.innerHTML = "Subject can't be empty"
+    } else {
+        errorSubject.innerHTML = ""
+    }
+    if (message.value.length == 0){
+        errorMessage.innerHTML = "Message can't be empty"
+    } else {
+        errorMessage.innerHTML = ""
     }
 }
