4d59dd6b13e5f4febaf39f1f254bd88495166255
elyssadg <elyssa.davina@gmail.com>
2022-06-16 22:07:16 +0700

    edit

--- script/slider.js
+++ script/slider.js
@@ -1,19 +1,75 @@
-// let currentSlide = 1;
-// showSlides(currentSlide);
+var counter = 1;
+var flag = false;
+var currentRadio;
 
-// function moveSlides(n) {
-//     showSlides(currentSlide += n);
-// }
+function moveSlides(n) {
+    if (slide)
+}
 
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("slide");
-//     if (n > slides.length) {currentSlide = 1}
-//     if (n < 1) {currentSlide = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slides[currentSlide-1].style.display = "block";
-// }
 
-var count
+function radioClicked(current, newRadio){
+    $(current).css("background", "")
+    $(newRadio).css("background", "rgb(121, 46, 0)")
+    currentRadio = $(newRadio);
+}
+
+function clicked(){
+    radioClicked(currentRadio, '#manual-btn-1');
+
+    $('#radio-1').click(function(){
+        counter = 1;
+        flag = true;
+        setTimeout(function(){
+            flag = false; 
+        }, 5000)
+        radioClicked(currentRadio, '#manual-btn-1');
+    })
+
+    $('#radio-2').click(function(){
+        counter = 2;
+        flag = true;
+        setTimeout(function(){
+            flag = false; 
+        }, 5000)
+        radioClicked(currentRadio, '#manual-btn-2');
+    })
+
+    $('#radio-3').click(function(){
+        counter = 3;
+        flag = true;
+        setTimeout(function(){
+            flag = false; 
+        }, 5000)
+        radioClicked(currentRadio, '#manual-btn-3');
+    })
+
+    document.getElementById('radio-' + counter).checked = true;
+
+    $('.next-btn').click(function(){
+        flag = true;
+        counter += 1;
+        if (counter > 3){
+            counter = 1;
+        }
+        radioClicked(currentRadio, '#manual-btn-' + counter);
+        document.getElementById('radio-' + counter).checked = true;
+        console.log(counter)
+        setTimeout(function(){
+            flag = false; 
+        }, 5000) 
+    });
+   
+    $('.prev-btn').click(function(){
+        flag = true;
+        counter--;
+        if (counter < 1){
+            counter = 3;
+        }
+        radioClicked(currentRadio, '#manual-btn-' + counter);
+        document.getElementById('radio-' + counter).checked = true;
+        console.log(counter)
+        setTimeout(function(){
+            flag = false; 
+        }, 5000)
+    });
+});
