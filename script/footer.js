6a9d7fb5ce9f6bf46d706f21c27cf29a89446de3
elyssadg <elyssa.davina@gmail.com>
2022-06-16 11:12:17 +0700

    edit

--- script/footer.js
+++ script/footer.js
@@ -4,16 +4,21 @@ let nameError = document.getElementById("name-error-message");
 let emailError = document.getElementById("email-error-message");
 
 function validateSubscribeForm(){
-    alert("Hello! I am an alert box!!");
+    var result = true
+
     if (username.value.length < 2){
         nameError.innerHTML = "Username is shorter than 2 characters"
+        result = false
     } else {
         nameError.innerHTML = ""
     }
 
     if (email.value.endsWith("@gmail.com") == false) {
         emailError.innerHTML = "Email must end with @gmail.com"
+        result = false
     } else {
         emailError.innerHTML = ""
     }
+
+    return result
 }
