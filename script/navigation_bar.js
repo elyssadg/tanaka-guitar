3d1f8c8ef5e74419d49ca7b062b5458e9f193c9b
elyssadg <elyssa.davina@gmail.com>
2022-06-16 10:18:03 +0700

    edit

--- script/navigation_bar.js
+++ script/navigation_bar.js
@@ -7,3 +7,8 @@ function closeSideBar(){
     document.getElementById("sidebar").style.width = "0";
     document.getElementsByClassName(".open-sidebar").style.display = "block";
 }
+
+window.addEventListener("scroll", function(){
+    var navbar = document.querySelector("navbar");
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+});
