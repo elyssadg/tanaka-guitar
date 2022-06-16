5f408b3cd8fc410e63a71f8d278b62774b2ce193
guidowilliam11 <guido.william@binus.ac.id>
2022-06-16 21:48:01 +0700

    update

--- script/slider.js
+++ script/slider.js
@@ -1,17 +1,19 @@
-let currentSlide = 1;
-showSlides(currentSlide);
+// let currentSlide = 1;
+// showSlides(currentSlide);
 
-function moveSlides(n) {
-    showSlides(currentSlide += n);
-}
+// function moveSlides(n) {
+//     showSlides(currentSlide += n);
+// }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {currentSlide = 1}
-    if (n < 1) {currentSlide = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[currentSlide-1].style.display = "block";
-}
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("slide");
+//     if (n > slides.length) {currentSlide = 1}
+//     if (n < 1) {currentSlide = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[currentSlide-1].style.display = "block";
+// }
+
+var count
