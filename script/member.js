5f1496400a865663501ea9bc0cb2643c547bb058
guidowilliam11 <guido.william@binus.ac.id>
2022-06-16 20:46:21 +0700

    update

--- script/member.js
+++ script/member.js
@@ -5,6 +5,7 @@ let email = document.getElementById("email");
 let gender = document.getElementById("gender");
 let agree = document.getElementById("terms-condition");
 
+<<<<<<< Updated upstream
 rform.addEventListener('submit',(event)=>{
     event.preventDefault();
     validateMemberForm();
@@ -18,6 +19,14 @@ function validateMemberForm(){
     } else {
         errorFirst.innerHTML = "";
         count += 1;
+=======
+function validateForm(){
+    alert("buton work");
+    if(first.value.length==0){
+        errorFirst.innerHTML = "First name can't be empty"
+    }else{
+        errorFirst.innerHTML = ""
+>>>>>>> Stashed changes
     }
     if (last.value.length == 0){
         errorLast.innerHTML = "Last name can't be empty";
